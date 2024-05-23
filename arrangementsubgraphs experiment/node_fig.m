ebd5bc251fc9d29d3ab11b77b12101ebfd4a27d1
zhangjian ji <zj8455@126.com>
2024-05-23 15:10:20 +0800

    Add files via upload

--- arrangementsubgraphs experiment/node_fig.m
+++ arrangementsubgraphs experiment/node_fig.m
@@ -13,20 +13,20 @@ load('E:\rmses752.mat')
 load('E:\rmses753.mat')
 load('E:\rmsesall.mat')
 %%
-plot(5:5:150,mean(rmses541,2)',':*','LineWidth',0.25);
+plot(5:5:100,mean(rmses541,2)',':*','LineWidth',0.25);
 hold on;
-plot(5:5:150,mean(rmses542,2)','-h','LineWidth',0.25);
-plot(5:5:150,mean(rmses543,2)','-c','LineWidth',0.25);
-plot(5:5:150,mean(rmses641,2)','-d','LineWidth',0.25);
-plot(5:5:150,mean(rmses642,2)','-x','LineWidth',0.25);
-plot(5:5:150,mean(rmses643,2)','-h','LineWidth',0.25);
-plot(5:5:150,mean(rmsesall,2)','-h','LineWidth',2);
-plot(5:5:150,mean(rmses651,2)',':+','LineWidth',0.25);
-plot(5:5:150,mean(rmses652,2)','-o','LineWidth',0.25);
-plot(5:5:150,mean(rmses653,2)','-h','LineWidth',0.25);
-plot(5:5:150,mean(rmses751,2)','-h','LineWidth',0.25);
-plot(5:5:150,mean(rmses752,2)','-h','LineWidth',0.25);
-plot(5:5:150,mean(rmses753,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmses542,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmses543,2)','-c','LineWidth',0.25);
+plot(5:5:100,mean(rmses641,2)','-d','LineWidth',0.25);
+plot(5:5:100,mean(rmses642,2)','-x','LineWidth',0.25);
+plot(5:5:100,mean(rmses643,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmsesall,2)','-h','LineWidth',2);
+plot(5:5:100,mean(rmses651,2)',':+','LineWidth',0.25);
+plot(5:5:100,mean(rmses652,2)','-o','LineWidth',0.25);
+plot(5:5:100,mean(rmses653,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmses751,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmses752,2)','-h','LineWidth',0.25);
+plot(5:5:100,mean(rmses753,2)','-h','LineWidth',0.25);
 
 hleg=legend('\itD\rm(\itA_5_,_4^1\rm)','\itD\rm(\itA_5_,_4^2\rm)','\itD\rm(\itA_5_,_4^3\rm)','\itD\rm(\itA_6_,_4^1\rm)','\itD\rm(\itA_6_,_4^2\rm)','\itD\rm(\itA_6_,_4^3\rm)','merged dataset','\itD\rm(\itA_6_,_5^1\rm)','\itD\rm(\itA_6_,_5^2\rm)','\itD\rm(\itA_6_,_5^3\rm)','\itD\rm(\itA_7_,_5^1\rm)','\itD\rm(\itA_7_,_5^2\rm)','\itD\rm(\itA_7_,_5^3\rm)');
 set(hleg,'FontName','Times New Roman');
